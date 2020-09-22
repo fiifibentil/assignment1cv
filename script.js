42d2516bb6ece7104b2c35e065b9166284a9217b
fiifibentil <fiifibentil@yahoo.com>
2020-09-22 15:35:17 +0000

    changes

--- script.js
+++ script.js
@@ -27,10 +27,72 @@ function editinfo() {
 
     document.getElementById("cvname").innerHTML= '<strong> '+ fullname +' </strong> ' +' <br> '+ Address+' <br> ' + Phonenumber+' <br> '+Email;
     
-    
-    
-    
     return;
     } 
 
+function check(){
+    const firstname= document.getElementById('fname').value;
+    const middlename= document.getElementById('mname').value;
+    const lastname= document.getElementById('lname').value;
+    //const phonenumber= document.getElementById('number').value;
+    const email= document.getElementById('email').value;
+    //const address= document.getElementById('address').value;
+
+    const checkname = /^[A-Za-z]{3,30}$/;
+    const checkemail = /^[A-Za-z_.]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z.]{2,6}$/;
+    const checknumber = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/;
+    const checkaddress = /^[#.0-9a-zA-Z\s,-]+$/;
+
+    if(checkname.test(firstname)){
+        document.getElementById('errorfname').innerHTML=" ";
+    }
+    else{
+        document.getElementById("errorfname").innerHTML="The name you entered is invalid. <br>";
+        return false;
+    }
+
+    if(checkname.test(middlename)){
+        document.getElementById('errormname').innerHTML=" ";
+    }
+    else{
+        document.getElementById("errormname").innerHTML="The name you entered is invalid. <br>";
+        return false;
+    }
+
+    if(checkname.test(lastname)){
+        document.getElementById('errorlname').innerHTML=" ";
+    }
+    else{
+        document.getElementById("errorlname").innerHTML="The name you entered is invalid. <br>";
+        return false;
+    }
+
+    /*if(checkname.test(phonenumber)){
+        document.getElementById('errornumber').innerHTML=" ";
+    }
+    else{
+        document.getElementById("errornumber").innerHTML="The phone number you entered is invalid. <br>";
+        return false;
+    }*/
+
+    if(checkemail.test(email)){
+        document.getElementById('erroremail').innerHTML=" ";
+    }
+    else{
+        document.getElementById("erroremail").innerHTML="The email you entered is invalid. <br>";
+        return false;
+    }
+
+    /*if(checkname.test(address)){
+        document.getElementById('erroraddress').innerHTML=" ";
+    }
+    else{
+        document.getElementById("erroraddress").innerHTML="The address you entered is invalid. <br>";
+        return false;
+    }*/
+
+
+
+
+}
 
